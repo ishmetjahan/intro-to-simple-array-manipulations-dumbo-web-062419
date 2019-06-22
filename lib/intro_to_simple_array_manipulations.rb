edc85324cea66f84d9f692afe80b327fdec033ae
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 01:44:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,4 +18,17 @@ def using_shift (arr)
   return arr.shift()
 end
 #
-def
+def shift_with_args (arr)
+  return arr.shift(2)
+end
+#
+def using_concat (arr1,arr2)
+  arr1.concat
+end
+
+
+
+
+  
+  
+  
